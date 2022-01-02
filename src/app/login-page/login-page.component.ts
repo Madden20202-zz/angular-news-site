43120d8a865c5304e6bc6a516c995f04dc6d3202
madden20202 <business.mocksock@gmail.com>
2022-01-02 11:30:41 -0800

    add home screen route

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from '../app.component';
 import { LoginScreenComponent } from '../login-screen/login-screen.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 import { SignupScreenComponent } from '../signup-screen/signup-screen.component';
@@ -14,6 +15,7 @@ export class LoginPageComponent implements OnInit {
   loginRoutes: Routes = [
     { path: 'login', component: LoginScreenComponent },
     { path: 'sign-up', component: SignupScreenComponent},
+    { path: 'home', component: AppComponent},
     { path: '**', component: PageNotFoundComponent }
   ]
 
