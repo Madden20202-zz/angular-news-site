7a7331f4dc910760632bf6f15bf059a8087e9a33
madden20202 <business.mocksock@gmail.com>
2022-01-21 09:30:46 -0800

    fix error with routes and rename

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { SignupScreenComponent } from './signup-screen/signup-screen.component';
 import { AppComponent } from './app.component';
 
-const loginRoutes: Routes = [
+const routes: Routes = [
   { path: 'login', component: LoginScreenComponent },
-    { path: 'sign-up', component: SignupScreenComponent},
-    { path: 'home', component: AppComponent},
-    { path: '**', component: PageNotFoundComponent }
+  { path: 'sign-up', component: SignupScreenComponent},
+  { path: 'home', component: AppComponent},
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
