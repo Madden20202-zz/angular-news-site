d5b9c932c6eef35bcff75015456e303918d759e4
madden20202 <business.mocksock@gmail.com>
2022-01-21 09:28:33 -0800

    after doing multiple tutorials, the module is set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignupScreenComponent } from './signup-screen/signup-screen.component';
+import { AppComponent } from './app.component';
+
+const loginRoutes: Routes = [
+  { path: 'login', component: LoginScreenComponent },
+    { path: 'sign-up', component: SignupScreenComponent},
+    { path: 'home', component: AppComponent},
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from '../app.component';
-import { LoginScreenComponent } from '../login-screen/login-screen.component';
-import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
-import { SignupScreenComponent } from '../signup-screen/signup-screen.component';
 
 @Component({
   selector: 'app-login-page',
@@ -11,13 +6,6 @@ import { SignupScreenComponent } from '../signup-screen/signup-screen.component'
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-
-  appRoutes: Routes = [
-    { path: 'login', component: LoginScreenComponent },
-    { path: 'sign-up', component: SignupScreenComponent},
-    { path: 'home', component: AppComponent},
-    { path: '**', component: PageNotFoundComponent }
-  ];
 
 
   constructor(){ }
