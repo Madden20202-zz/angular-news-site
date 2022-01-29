c109672ec315e28e79532d0b76c65d586a64c86f
madden20202 <business.mocksock@gmail.com>
2022-01-29 15:18:54 -0800

    add documentation

--- src/app/signup-screen/signup-screen.component.ts
+++ src/app/signup-screen/signup-screen.component.ts
@@ -19,14 +19,18 @@ export class SignupScreenComponent implements OnInit {
   password = new FormControl('', Validators.required);
   cowName = new FormControl('', Validators.required);
 
+  // this will be used for when the backend is updating
+  // ideally the firebase used will be fast and not need this
   loading = false;
 
-
+  // should study what this does
   constructor() { }
 
+  // check if the values need to be set on init
   ngOnInit(): void { }
 
   onSubmit() {
+    // later this will be used to update the backend values
     console.log("The submit button works");
   }
 }
