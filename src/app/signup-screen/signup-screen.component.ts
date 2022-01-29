14346d9782020b72264cd2bc2e96d4aabd192493
madden20202 <business.mocksock@gmail.com>
2022-01-29 15:12:04 -0800

    add required validator

--- src/app/signup-screen/signup-screen.component.ts
+++ src/app/signup-screen/signup-screen.component.ts
@@ -15,17 +15,16 @@ import { User } from 'user/user.model';
 export class SignupScreenComponent implements OnInit {
 
   // all form controls
-  username = new FormControl('');
-  password = new FormControl('');
-  cowName = new FormControl('');
+  username = new FormControl('', Validators.required);
+  password = new FormControl('', Validators.required);
+  cowName = new FormControl('', Validators.required);
 
   loading = false;
 
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   onSubmit() {
     console.log("The submit button works");
