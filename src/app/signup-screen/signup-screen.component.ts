4b23f7abe6755122eb2ac08fb1f57563bde05161
madden20202 <business.mocksock@gmail.com>
2022-01-29 14:31:15 -0800

    changed code to proper file

--- src/app/signup-screen/signup-screen.component.ts
+++ src/app/signup-screen/signup-screen.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  FormBuilder, 
+  FormGroup, 
+  Validators, 
+  FormControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup-screen',
@@ -6,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-screen.component.css']
 })
 export class SignupScreenComponent implements OnInit {
+  
+  // all form controls
+  username = new FormControl('');
+  password = new FormControl('');
+  cowName = new FormControl('');
 
-  constructor() { }
+  loading = false;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log("The submit button works");
+  }
 }
