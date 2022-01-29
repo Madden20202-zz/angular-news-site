e13ed07a178d72f83dbdd11b2a5448c41fb4dccd
madden20202 <business.mocksock@gmail.com>
2022-01-29 10:38:55 -0800

    add another import and set up basics for a rf

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { 
+  FormBuilder, 
+  FormGroup, 
+  Validators, 
+  FormControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.css']
 })
+
 export class LoginScreenComponent implements OnInit {
 
+  loginForm = new FormControl('');
+  loading = false;
+  newUserSubmission = false;
 
-  
-  constructor() { }
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log("The submit button works");
+  }
 }
