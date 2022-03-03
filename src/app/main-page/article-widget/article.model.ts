7308fe53e4c0adf64096711d0436a5fdfc7c2a1a
austin <business.mocksock@gmail.com>
2022-03-03 10:13:26 -0700

    add up/down vote and update

--- src/app/main-page/article-widget/article.model.ts
+++ src/app/main-page/article-widget/article.model.ts
@@ -5,4 +5,21 @@ import { User } from "user/user.model";
     content = new String();
     author = User;
     popular = true;
+
+    updateArticle(/*figure out what is needed here*/) {
+        // This would update up/down votes, comments, etc
+        console.log("I would allow the article to update!");
+    }
+
+    downVote(popular: boolean) {
+        popular = false;
+        return popular;
+    }
+
+    upVote(popular: boolean) {
+        popular = true;
+        return popular;    
+    }
+
+    
 }
