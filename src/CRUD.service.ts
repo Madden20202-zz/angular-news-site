9f419b3a301828d37bc708f29ab618b24eda64e4
austin <business.mocksock@gmail.com>
2022-03-07 10:42:30 -0700

    move article post code over to CRUD service

---
+++ src/CRUD.service.ts
@@ -0,0 +1,40 @@
+import { User } from "user/user.model";
+
+ export class articlePost{
+    title = new String();
+    content = new String();
+    author = User;
+    popular = true;
+    searchTag = new String();
+    date = new Date();
+
+    createArticle(value: any) {
+        this.postdata = {
+            title: this.title.valueOf(),
+            author: this.author.valueOf(),
+            searchTags: this.searchTag.valueOf(),
+            content: this.content.valueOf(),
+            popular: this.popular.valueOf(),
+            date: this.date.valueOf(),
+        }
+    }
+
+    // Work on moving these to their own file
+    // this is making a super class
+    updateArticle(/*figure out what is needed here*/) {
+        // This would update up/down votes, comments, etc
+        console.log("I would allow the article to update!");
+    }
+
+    downVote(popular: boolean) {
+        popular = false;
+        return popular;
+    }
+
+    upVote(popular: boolean) {
+        popular = true;
+        return popular;    
+    }
+
+
+}
